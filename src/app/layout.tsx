b7da81c2d3c56659785e5d1078397d85378b583e
João Vitor Olsen <joao.vitor@inovaebiz.com.br>
2023-10-15 08:46:20 -0300

    SEO

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 import './globals.css';
 import type { Metadata } from 'next';
 import { Inter } from 'next/font/google';
+import Head from 'next/head';
 
 const inter = Inter({ subsets: ['latin'] });
 
@@ -17,6 +18,40 @@ export default function RootLayout({
 }) {
   return (
     <html lang="pt-BR">
+      <Head>
+        <meta charSet="UTF-8" />
+        <meta
+          name="viewport"
+          content="width=device-width, initial-scale=1.0"
+        />
+        <meta
+          name="description"
+          content="Show how much time in days, weeks and months until Bitcoin next halving."
+        />
+
+        <meta
+          property="og:title"
+          content="Bitcoin Halving"
+        />
+        <meta
+          property="og:description"
+          content="Show how much time in days, weeks and months until Bitcoin next halving."
+        />
+        <meta
+          property="og:image"
+          content="https://avatars.githubusercontent.com/u/528860?s=200&v=4"
+        />
+        <meta
+          property="og:url"
+          content="https://weeksuntilhalving.live"
+        />
+        <meta property="og:type" content="website" />
+
+        <link
+          rel="canonical"
+          href="https://weeksuntilhalving.live"
+        />
+      </Head>
       <body className={inter.className}>{children}</body>
     </html>
   );
